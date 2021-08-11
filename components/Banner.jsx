1c6d14ee5cccb3f1bc66a85d9bfa807b58e400b5
dilip stack <designsdilip@gmail.com>
2021-08-12 00:24:15 +0545

    production bug

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,7 +11,7 @@ priority={true}
 />
 <div className='absolute top-1/3  w-[230px] text-left ml-[50px] lg:ml-28'>
     <p className='text-sm sm:text-lg text-white font-bold xl:text-[30px] xl:w-[400px]'>Not sure where to go? Perfect.</p>
-    <button className='text-purple-500 bg-white px-5 py-[8px] shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 xl:mt-[25px]'>I'm flexible</button>
+    <button className='text-purple-500 bg-white px-5 py-[8px] shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 xl:mt-[25px]'>I&apos;m flexible</button>
 </div>
         </div>
     )
